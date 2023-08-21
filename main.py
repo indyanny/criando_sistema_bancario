194e9198694adfe66e1a6aabbe2b50501eedfaf1
indyanny <indy.engenharia@gmail.com>
2023-08-21 14:00:12 -0300

    criando_sistema_bancario

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-menu = ("[1]Saque [2]Extrato [3]Depósito [0]Sair \n Informe a opção desejada:")
+menu = ("[1]Depósito [2]Saque [3]Extrato [0]Sair \n Informe a opção desejada:")
 
-[1] == "Saque" 
-[2] == "Extrato"
-[3] == "Deposito"
+[1] == "Depósito" 
+[2] == "Saque"
+[3] == "Extrato"
 [0] == "Sair"
 
 
 saldo = 0
-limite = 1500
+limite = 500
 extrato = "saldo"
-numero_saques = 3
-LIMITE_SAQUE = 1500
+numero_saques = 4
+LIMITE_SAQUE = 2000
 
 while True:
     opcao = input(menu)
 
     if opcao == "1":
+        valor = float(input("Informe o valor do depósito:"))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor: .2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido!")
+
+    elif opcao == "2":
         valor = float(input("Informe o valor de saque:"))
 
         excedeu_saldo = valor > saldo
@@ -41,21 +51,11 @@ while True:
         else:
             print("Operação falhou! O valor informado é inválido!")
 
-    elif opcao == "2":
+    elif opcao == "3":
         print("\n==============EXTRATO==============")
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"\n Saldo: R$ {valor: .2f}")
         print("======================================")
-
-    elif opcao == "3":
-        valor = float(input("Informe o valor do depósito:"))
-
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor: .2f}\n"
-
-        else:
-            print("Operação falhou! O valor informado é inválido!")
 
     elif opcao == "0":
         break
